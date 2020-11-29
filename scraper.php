100b1c9adeefcb9821fb45d92b463ec60288fd1e
BenSaxon2020 <75187277+BenSaxon2020@users.noreply.github.com>
2020-11-29 15:10:30 +0000

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -1,27 +1,28 @@
 <?php
-// $html = file_get_contents('https://stackoverflow.com/questions/21741557/take-text-from-another-website-through-php');
-
-$html = file_get_contents('https://www.amazon.co.uk/dp/B07X37DT9M/ref=gw_uk_desk_mso_vicc_owl?pf_rd_r=7BJSBVZJJT8JCYGZ5RQG&pf_rd_p=c981c9b3-d77e-4e0b-9edf-f42e59718556'); //get the html returned from the following url
+$html = file_get_contents('[<enter url>]'); //get the html returned from the following url
 
 
 $doc1 = new DOMDocument();
 
 libxml_use_internal_errors(TRUE); //disable libxml errors
 
-if(!empty($html)){ //if any html is actually returned
+if(!empty($html)){ //if any html is returned
 
 	$doc1->loadHTML($html);
-	libxml_clear_errors(); //remove errors for yucky html
+	libxml_clear_errors(); //remove errors 
 	
 	$xmen = new DOMXPath($doc1);
 
 	//get all the h2's with an id
-	$xmen_11 = $xmen->query('//span[@id="price_inside_buybox"]');
+	$xmen_11 = $xmen->query('//span[@id="<enter html element id you want>"]');
 
 	if($xmen_11->length > -1){
 		foreach($xmen_11 as $row){
 			echo $row->nodeValue . "<br/>";
-		}
-	}
+        }
+    }
+    else{
+        echo "suck ur mum";
+    }
 }
 
